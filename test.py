139bf1b67ea9d2a9bb5476f41358b788e8594b19
Pu5hinka <pu5hinka0692@gmail.com>
2022-11-09 17:05:52 +0300

    initial commit

---
+++ test.py
@@ -0,0 +1,44 @@
+# num = int(input('Введите число'))
+# if num % 2==0:
+#     print('четное число', num)
+# else:
+#     print('нечетное число')
+
+# letter = input('Введите текст')
+# if letter == 'a' or letter == 'e' or letter == 'i':
+#     print('глассная буква')
+# elif letter == 'y':
+#     print('согласная и гласная')
+# else:
+#     print('согласная')
+
+# for i in range(0, 11):
+#     if i % 2 == 0:
+#         print(i, end=' ')
+
+# num = int(input('введи целое число'))
+# data = []
+# while num != 0:
+#     data.append(num)
+#     num = int(input('введи целое число'))
+# data.sort()
+# print(data)
+
+# words = input('введи список слов')
+# data = []
+# while words != "":
+#     if words not in data:
+#         data.append(words)
+#     words = input()
+# for item in data:
+#     print(item)
+# print(data)
+a = 5
+b = 3+2
+
+print(id(a))
+print(id(b))
+
+
+
+
